0edbbfc602240e4358287b57dea87c44fcde0411
Dale E. Moore <DaleEMoore@gMail.Com>
2014-08-25 08:06:40 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# django-basic-apps-MW for using https://github.com/nathanborror/django-basic-apps
+# Download https://github.com/nathanborror/django-basic-apps
+# python setup.py install
+
+# mkdir ~/dbaTest
+# cd dbaTest
+# copy djang-basic-apps in
+# python manage.py runserver 0.0.0.0:8000
+# http://localhost:8000/dbaTest
+
+__author__ = 'dalem'
+
